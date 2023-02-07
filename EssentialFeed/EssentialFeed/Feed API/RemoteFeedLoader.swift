5a746d20625f6d9f7368ae2b284f18ab99f38ee2
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-02-07 14:36:57 +0400

    Switch to `Result` enum value for `RemoteFeedLoader` completion block

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -13,6 +13,7 @@ public protocol HTTPClient {
 	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
+
 public final class RemoteFeedLoader {
 	private let url: URL
 	private let client: HTTPClient
@@ -22,18 +23,20 @@ public final class RemoteFeedLoader {
 		case invalidData
 	}
 
+	public typealias Result = Swift.Result<[FeedItem], Error>
+
 	public init(url: URL, client: HTTPClient) {
 		self.url = url
 		self.client = client
 	}
 
-	public func load(completion: @escaping (Error) -> Void) {
+	public func load(completion: @escaping (Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
 			case .success:
-				completion(.invalidData)
+				completion(.failure(.invalidData))
 			case .failure:
-				completion(.connectivity)
+				completion(.failure(.connectivity))
 			}
 		}
 	}
