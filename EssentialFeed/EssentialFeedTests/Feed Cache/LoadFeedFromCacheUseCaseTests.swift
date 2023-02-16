4ef49900bdce29811b5fb763c1d54a4700b87492
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-02-16 19:19:23 +0400

    Load command does not deliver load result after the instance has been deallocated to prevent unexpected behaviours in clients code

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -128,6 +128,21 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
 	}
 
+	func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+		let store = FeedStoreSpy()
+		var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+
+		var receivedResults: [LocalFeedLoader.LoadResult] = []
+		sut?.load { receivedResult in
+			receivedResults.append(receivedResult)
+		}
+
+		sut = nil
+		store.completeWithEmptyCache()
+
+		XCTAssertTrue(receivedResults.isEmpty)
+	}
+
 	// MARK: - Helpers
 	private func makeSUT(
 		currentDate: @escaping () -> Date = Date.init,
