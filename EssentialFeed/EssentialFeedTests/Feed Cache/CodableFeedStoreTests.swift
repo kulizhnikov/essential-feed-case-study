c9485cf911a1b65cc4e6146ec8caf391b48d79cc
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-02-19 07:27:26 +0400

    Make `CodableFeedStore` conform to `FeedStore`

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class CodableFeedStore {
+class CodableFeedStore: FeedStore {
 	private struct Cache: Codable {
 		let feed: [CodableFeedImage]
 		let timestamp: Date
@@ -42,7 +42,7 @@ class CodableFeedStore {
 		self.storeURL = storeURL
 	}
 
-	func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+	func retrieve(completion: @escaping RetrievalCompletion) {
 		guard let data = try? Data(contentsOf: storeURL) else {
 			completion(.empty)
 			return
@@ -58,7 +58,7 @@ class CodableFeedStore {
 		}
 	}
 
-	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		do {
 			let encoder = JSONEncoder()
 			let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
@@ -71,7 +71,7 @@ class CodableFeedStore {
 
 	}
 
-	func delete(completion: @escaping FeedStore.DeletionCompletion) {
+	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		guard FileManager.default.fileExists(atPath: storeURL.path) else {
 			completion(nil)
 			return
@@ -230,7 +230,7 @@ final class CodableFeedStoreTests: XCTestCase {
 	private func deleteCache(from sut: CodableFeedStore) -> Error? {
 		let exp = expectation(description: "Wait for cache deletion")
 		var deletionError: Error?
-		sut.delete() { receivedDeletionError in
+		sut.deleteCachedFeed() { receivedDeletionError in
 			deletionError = receivedDeletionError
 			exp.fulfill()
 		}
