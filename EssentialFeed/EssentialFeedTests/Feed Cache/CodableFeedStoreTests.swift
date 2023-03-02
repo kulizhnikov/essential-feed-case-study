9e3691a7dfe86e85a98e43413ad5d2a710626843
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-03 00:51:03 +0600

    Extract test assertions to reusable `FailableInsertionFeedStoreSpec`

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -86,17 +86,14 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
 		let invalidStoreURL = URL(string: "invalid://store-url")!
 		let sut = makeSUT(storeURL: invalidStoreURL)
 
-		let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
-		XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+		assertThatInsertDeliversFailureOnInsertionError(on: sut)
 	}
 
 	func test_insert_hasNoSideEffectsOnInsertionError() {
 		let invalidStoreURL = URL(string: "invalid://store-url")!
 		let sut = makeSUT(storeURL: invalidStoreURL)
 
-		insert((uniqueImageFeed().local, Date()), to: sut)
-
-		expect(sut, toRetrieve: .empty)
+		assertThatInsertHasNoSideEffectsOnFailure(on: sut)
 	}
 
 	func test_delete_deliversNoErrorOnEmptyCache() {
---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -0,0 +1,30 @@
+//
+//  XCTestCase+FailableRetrieveFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Dmitry Kulizhnikov on 03.03.2023.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
+	func assertThatInsertHasNoSideEffectsOnFailure(
+		on sut: FeedStore,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) {
+		insert((uniqueImageFeed().local, Date()), to: sut)
+
+		expect(sut, toRetrieveTwice: .empty)
+	}
+
+	func assertThatInsertDeliversFailureOnInsertionError(
+		on sut: FeedStore,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) {
+		let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+		XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+	}
+}
