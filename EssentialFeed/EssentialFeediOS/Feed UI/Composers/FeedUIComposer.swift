f3f3205cc18f1473e768bdc29b5938f556bc7035
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-12 03:06:28 +0600

    Create `FeedImageViewModel`

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -31,7 +31,8 @@ public final class FeedUIComposer {
 
 		return { [weak controller] feed in
 			controller?.tableModel = feed.map { model in
-				FeedImageCellController(model: model, imageLoader: loader)
+				let viewModel = FeedImageViewModel(model: model, imageLoader: loader)
+				return FeedImageCellController(viewModel: viewModel)
 			}
 		}
 
