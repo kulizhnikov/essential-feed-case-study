d6cd269ec493c8ade3174da0da7b3ef0e612a411
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-17 02:31:19 +0600

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Dmitry Kulizhnikov on 17.03.2023.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+	public let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Dmitry Kulizhnikov on 17.03.2023.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+	public let feed: [FeedImage]
+}
