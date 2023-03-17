1e167264bfa833bfa11e0d2cab23e18ed53339d8
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-18 00:23:57 +0600

    Extract duplicate HTTPClientSpy helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -206,36 +206,4 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
 	private func failure(_ error: RemoteFeedImageDataLoader.Error) -> FeedImageDataLoader.Result {
 		return .failure(error)
 	}
-
-	private class HTTPClientSpy: HTTPClient {
-		private struct Task: HTTPClientTask {
-			func cancel() { }
-		}
-
-		private var messages: [(url: URL, completion: (HTTPClient.Result) -> Void)] = []
-		private var cancelledURLs: [URL] = []
-
-		var requestedURLs: [URL] {
-			return messages.map { $0.url }
-		}
-
-		func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-			messages.append((url, completion))
-			return Task()
-		}
-
-		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(.failure(error))
-		}
-
-		func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-			let response = HTTPURLResponse(
-				url: requestedURLs[index],
-				statusCode: code,
-				httpVersion: nil,
-				headerFields: nil
-			)!
-			messages[index].completion(.success((data, response)))
-		}
-	}
 }
