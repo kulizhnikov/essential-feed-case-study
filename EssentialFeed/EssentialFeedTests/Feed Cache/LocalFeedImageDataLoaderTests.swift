290873b95721f4210911109d1eeeccb7994421eb
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-18 12:00:50 +0600

    `LocalFeedImageDataLoader.loadImageData` does not deliver result after cancelling task

--- EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -15,8 +15,24 @@ protocol FeedImageDataStore {
 }
 
 final class LocalFeedImageDataLoader: FeedImageDataLoader {
-	private struct Task: FeedImageDataLoaderTask {
-		func cancel() { }
+	private final class Task: FeedImageDataLoaderTask {
+		var completion: ((FeedImageDataLoader.Result) -> Void)?
+
+		init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+			self.completion = completion
+		}
+
+		func complete(with result: FeedImageDataLoader.Result) {
+			completion?(result)
+		}
+
+		func cancel() {
+			preventFutureCompletions()
+		}
+
+		private func preventFutureCompletions() {
+			completion = nil
+		}
 	}
 
 	public enum Error: Swift.Error {
@@ -34,14 +50,16 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
 		from url: URL,
 		completion: @escaping (FeedImageDataLoader.Result) -> Void
 	) -> EssentialFeed.FeedImageDataLoaderTask {
+		let task = Task(completion)
 
 		store.retrieve(dataForURL: url) { result in
-			completion(result
+			task.complete(with: result
 				.mapError { _ in Error.failed }
-				.flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound) }
-			)
+				.flatMap { data in
+					data.map { .success($0) } ?? .failure(Error.notFound)
+				})
 		}
-		return Task()
+		return task
 	}
 }
 
@@ -86,6 +104,21 @@ final class LocalFeedImageDataLoaderTests: XCTestCase {
 		expect(sut, toCompleteWith: .success(foundData), when: {
 			store.complete(with: foundData)
 		})
+	}
+
+	func test_loadImageDataFromURL_doesNotDeliverResultAfterCancellingTask() {
+		let (sut, store) = makeSUT()
+		let foundData = anyData()
+
+		var received: [FeedImageDataLoader.Result] = []
+		let task = sut.loadImageData(from: anyURL()) { received.append($0) }
+		task.cancel()
+
+		store.complete(with: foundData)
+		store.complete(with: .none)
+		store.complete(with: anyNSError())
+
+		XCTAssertTrue(received.isEmpty, "Expected no received results after cancelling task")
 	}
 
 	// MARK: - Helpers
