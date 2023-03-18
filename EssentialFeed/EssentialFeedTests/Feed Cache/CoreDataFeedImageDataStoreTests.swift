4911cb9071c942f865cdbdbdf19cdbe4f5322ab7
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-18 21:43:20 +0600

    Encapsulate `CoreDataStore` bundle creation into a centralized place to remove duplication and prevent mistakes

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -51,27 +51,26 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 	func test_sideEffects_runSerially() {
 		let sut = makeSUT()
 		let url = anyURL()
-		
+
 		let op1 = expectation(description: "Operation 1")
 		sut.insert([localImage(url: url)], timestamp: Date()) { _ in
 			op1.fulfill()
 		}
-		
+
 		let op2 = expectation(description: "Operation 2")
 		sut.insert(anyData(), for: url) { _ in	op2.fulfill() }
-		
+
 		let op3 = expectation(description: "Operation 3")
 		sut.insert(anyData(), for: url) { _ in op3.fulfill() }
-		
+
 		wait(for: [op1, op2, op3], timeout: 5.0, enforceOrder: true)
 	}
 
 	// - MARK: Helpers
 
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
-		let storeBundle = Bundle(for: CoreDataFeedStore.self)
 		let storeURL = URL(fileURLWithPath: "/dev/null")
-		let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+		let sut = try! CoreDataFeedStore(storeURL: storeURL)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}
