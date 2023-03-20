cbfc0a147f6df3843f0113afaa3ea1567b3d4a84
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-20 20:12:23 +0600

    `FeedLoaderWithFallbackComposite` delivers fallback result on primary failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -10,13 +10,25 @@ import EssentialFeed
 
 class FeedLoaderWithFallbackComposite: FeedLoader {
 	private let primary: FeedLoader
+	private let fallback: FeedLoader
 
 	init(primary: FeedLoader, fallback: FeedLoader) {
 		self.primary = primary
+		self.fallback = fallback
 	}
 
 	func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		primary.load(completion: completion)
+		primary.load() { [weak self] result in
+			guard let self = self else { return }
+			
+			switch result {
+			case .success:
+				completion(result)
+
+			case .failure:
+				self.fallback.load(completion: completion)
+			}
+		}
 	}
 }
 
@@ -41,7 +53,26 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 		}
 
 		wait(for: [exp], timeout: 1.0)
+	}
 
+	func test_load_deliversFallbackFeedOnPrimaryLoadFailure() {
+		let fallbackFeed = uniqueFeed()
+		let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+		let exp = expectation(description: "Wait for load completion")
+		sut.load { result in
+			switch result {
+
+			case let .success(receivedFeed):
+				XCTAssertEqual(receivedFeed, fallbackFeed)
+
+			case .failure:
+				XCTFail("Expected successful load feed result, got \(result) instead")
+			}
+			exp.fulfill()
+		}
+
+		wait(for: [exp], timeout: 1.0)
 	}
 
 	// MARK: - Helpers
@@ -66,7 +97,11 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 	private func uniqueFeed() -> [FeedImage] {
 		return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "https://any-url.com")!)]
 	}
-	
+
+	private func anyNSError() -> NSError {
+		return NSError(domain: "any error", code: 0)
+	}
+
 	private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
 		addTeardownBlock { [weak instance] in
 			XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
