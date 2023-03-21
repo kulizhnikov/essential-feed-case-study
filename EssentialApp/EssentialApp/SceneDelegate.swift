27aff33127478a8d591dc278d9f6f652c0107fba
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-22 00:07:22 +0600

    `FeedImageDataLoaderCacheDecorator.loadImageData` delivers decoratee result (either success or failure)

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -33,7 +33,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		let localImageLoader = LocalFeedImageDataLoader(store: localStore)
 
 		let feedViewController = FeedUIComposer.feedComposedWith(
-			feedLoader: FeedLoaderWithFallbackComposite(primary: remoteFeedLoader, fallback: localFeedLoader),
+			feedLoader: FeedLoaderWithFallbackComposite(
+				primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader),
+				fallback: localFeedLoader
+			),
 			imageLoader: FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: remoteImageLoader)
 		)
 
