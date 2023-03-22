48b2c9b2c5c4f05e65eea9454ad5bf68a0a1256d
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-22 22:43:26 +0600

    Move Feed UI composition details from the EssentialFeediOS to the EssentialApp module (Composition Root)

---
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -0,0 +1,47 @@
+//
+//  FeedUIComposer.swift
+//  EssentialFeediOS
+//
+//  Created by Dmitry Kulizhnikov on 08.03.2023.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+public final class FeedUIComposer {
+	private init() { }
+
+	public static func feedComposedWith(
+		feedLoader: FeedLoader,
+		imageLoader: FeedImageDataLoader
+	) -> FeedViewController {
+		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
+
+		let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
+
+		let presenter = FeedPresenter(
+			feedView: FeedViewAdapter(
+				controller: feedController,
+				imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)
+			),
+			loadingView: WeakRefVirtualProxy(feedController),
+			errorView: WeakRefVirtualProxy(feedController)
+		)
+		presentationAdapter.presenter = presenter
+
+		return feedController
+	}
+}
+
+private extension FeedViewController {
+	static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+		let bundle = Bundle(for: FeedViewController.self)
+		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+		let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+		feedController.delegate = delegate
+		feedController.title = title
+
+		return feedController
+	}
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -9,15 +9,15 @@ import Foundation
 import UIKit
 import EssentialFeed
 
-protocol FeedViewControllerDelegate {
+public protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
 }
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
-	var delegate: FeedViewControllerDelegate?
+	public var delegate: FeedViewControllerDelegate?
 	@IBOutlet private(set) public var errorView: ErrorView?
 	
-	var tableModel: [FeedImageCellController] = [] {
+	private var tableModel: [FeedImageCellController] = [] {
 		didSet {
 			tableView.reloadData()
 		}
@@ -31,6 +31,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
 	@IBAction private func refresh() {
 		delegate?.didRequestFeedRefresh()
+	}
+
+	public func display(_ cellControllers: [FeedImageCellController]) {
+		tableModel = cellControllers
 	}
 
 	public func display(_ viewModel: FeedLoadingViewModel) {		
