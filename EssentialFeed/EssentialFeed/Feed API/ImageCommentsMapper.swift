a8140ce5ed886b48991a949240749b9e656ead21
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-26 00:24:50 +0600

    Delivers proper result on 2xx response

--- EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -12,14 +12,16 @@ internal final class ImageCommentsMapper {
 		let items: [RemoteFeedItem]
 	}
 
-	private static let OK_200 = 200
-
 	internal static func map(_ data: Data, response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-		guard response.statusCode == OK_200,
+		guard isOK(response),
 			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			throw RemoteImageCommentsLoader.Error.invalidData
 		}
 
 		return root.items
+	}
+
+	private static func isOK(_ response: HTTPURLResponse) -> Bool {
+		(200...299).contains(response.statusCode)
 	}
 }
