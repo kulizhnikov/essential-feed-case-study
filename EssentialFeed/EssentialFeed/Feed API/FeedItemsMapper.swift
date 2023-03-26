2056e814f84564bd9b33afa6704493855111a666
Dmitry Kulizhnikov <kulizhnikov@gmail.com>
2023-03-26 15:08:07 +0600

    Move FeedImage mapping to the FeedImageMapper

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -8,18 +8,29 @@
 import Foundation
 
 internal final class FeedItemsMapper {
+	private struct RemoteFeedItem: Decodable {
+		internal let id: UUID
+		internal let description: String?
+		internal let location: String?
+		internal let image: URL
+	}
+
 	private struct Root: Decodable {
-		let items: [RemoteFeedItem]
+		private let items: [RemoteFeedItem]
+
+		var images: [FeedImage] {
+			items.map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
+		}
 	}
 
 	private static let OK_200 = 200
 
-	internal static func map(_ data: Data, response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+	internal static func map(_ data: Data, response: HTTPURLResponse) throws -> [FeedImage] {
 		guard response.statusCode == OK_200,
 			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			throw RemoteFeedLoader.Error.invalidData
 		}
 
-		return root.items
+		return root.images
 	}
 }
